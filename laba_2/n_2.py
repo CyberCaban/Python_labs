bf3055a3f7e3649c26f6a7210e90059632b429bb
CyberCaban <andrewgamer63@gmail.com>
2024-02-18 12:07:24 +0300

    second task complete

--- laba_2/n_2.py
+++ laba_2/n_2.py
@@ -1,5 +1,11 @@
 import csv
-from pprint import pprint
+from operator import itemgetter
+
+def write_csv(file_name: str, headers: list, data: list) -> None:
+    with open(file_name, "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=headers, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        writer.writerows(data)
 
 try:
     lower_income = int(input("Enter lower income in range: "))
@@ -8,17 +14,13 @@ except ValueError:
     print("enter integer please!!!")
     exit()
 
-incomes = []
-headers = []
 with open("countries.csv", "r") as file:
     reader = csv.DictReader(file, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
     headers = reader.fieldnames
-    for row in reader:
-        if lower_income < float(row["Income"]) and higher_income > float(row["Income"]):
-            incomes.append(row)
+    countries = list(reader)
 
-with open("incomes.csv", "w", newline="") as file:
-    writer = csv.DictWriter(file, fieldnames=headers, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
-    writer.writeheader()
-    writer.writerows(incomes)
-    
+inflation_sort = sorted(countries, key=itemgetter("Inflation"))
+incomes = list(filter(lambda row: lower_income < float(row["Income"]) and higher_income > float(row["Income"]), countries))
+
+write_csv("incomes.csv", headers, incomes)
+write_csv("sort_by_inflation.csv", headers, inflation_sort)
