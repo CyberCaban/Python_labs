b7d471c0ea100289cba61a647796f876f9171202
CyberCaban <andrewgamer63@gmail.com>
2024-02-18 12:07:53 +0300

    optimizon

--- laba_1/n_1.py
+++ laba_1/n_1.py
@@ -5,5 +5,5 @@ if len(words) != 10:
     print("Нужно ввести 10 слов!!!")
     exit()
 
-res = list(filter(lambda x: x != None, map(lambda word: word if word.startswith(ctrl) else None, words)))
+res = list(filter(lambda word: word.startswith(ctrl), words))
 print(res)
